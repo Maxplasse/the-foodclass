608da360abf1f6ca5a6c3fad584a8a2dcb4c15f7
marie sepanik <sepanik.marie@gmail.com>
2022-04-07 10:04:29 +0200

    routes et controller de courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,39 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @chef = Chef.find(params[:id])
+    @course = Course.new(course_params)
+    @course.chef = @chef
+    if @course.save
+      redirect_to new_course_path(@course)
+    else
+      render "courses/index"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :difficulty, :duration, :category, :starts_at, :ends_at, :chef_id)
+  end
+end
