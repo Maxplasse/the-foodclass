6d09188f7d2b8abae2bb25d9b0b2b82d864ce19b
marie sepanik <sepanik.marie@gmail.com>
2022-04-11 09:44:31 +0200

    index des cours

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
-    @courses = Course.all
+    @courses = policy_scope(Course).order(created_at: :desc)
   end
 
   def show
