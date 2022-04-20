6ddf67fa87afae0856a2cb6f61e4c46d167ee1a1
marie sepanik <sepanik.marie@gmail.com>
2022-04-20 14:57:46 +0200

    pg search index ok

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'pundit'
 # cloudinary
 gem 'cloudinary', '~> 1.16.0'
 
+# pgsearch
+gem 'pg_search', '~> 2.3.0'
+
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form', github: 'heartcombo/simple_form'
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :chef
+  has_one :user, through: :chef
   has_many :participations
   has_many :posts
 
@@ -8,4 +9,16 @@ class Course < ApplicationRecord
   validates :category, inclusion: { in: CATEGORIES }
 
   has_one_attached :photo
+
+  include PgSearch::Model
+
+  pg_search_scope :global_search,
+    against: [:title, :difficulty, :duration, :category, :starts_at],
+    associated_against: {
+      user: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+    # ignoring: :accents
 end
